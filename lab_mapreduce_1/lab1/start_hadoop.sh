e80ede5210acb4c5ed2e65646cdee6e3d0e7012d
MrKonstantin <mr.konstk@gmail.com>
2015-03-17 15:55:28 -0700

    lab1

--- lab_mapreduce_1/lab1/start_hadoop.sh
+++ lab_mapreduce_1/lab1/start_hadoop.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -input /user/cloudera/input/lab1/* -output /user/cloudera/output/lab2 -mapper /home/cloudera/projects/lab1/mapper.py -reducer /home/cloudera/projects/lab1/reducer.sh
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -input input/lab1/* -output output/lab1 -mapper ~/projects/lab_mapreduce_1/lab1/mapper.py -reducer ~/projects/lab_mapreduce_1/lab1/reducer.sh
+hadoop fs -cat output/lab1/* | head > head_result.dat
