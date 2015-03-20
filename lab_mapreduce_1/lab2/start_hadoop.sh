f668e66599f9c44860fc580cc39be6d86aa20aeb
MrKonstantin <mr.konstk@gmail.com>
2015-03-19 17:47:57 -0700

    lab2

--- lab_mapreduce_1/lab2/start_hadoop.sh
+++ lab_mapreduce_1/lab2/start_hadoop.sh
@@ -8,9 +8,5 @@ hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
     -output output/lab2/allDocCount \
     -mapper ~/projects/lab_mapreduce_1/lab2/mapper.py \
     -reducer ~/projects/lab_mapreduce_1/lab2/mapper.py \
-<<<<<<< HEAD
-    -D mapreduce.job.reduces=1
-=======
->>>>>>> b25cc6d9e56a9f7717a1e1470087acf34b4be651
 
 hadoop fs -cat output/lab2/result/* | head > head_result.dat
