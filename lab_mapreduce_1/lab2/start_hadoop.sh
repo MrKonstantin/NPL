ba2fb9f1cadc761d8e757b715b33bbfb8f963b88
kkiselev <mr.konstk@gmail.com>
2015-03-20 04:04:50 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	lab_mapreduce_1/lab2/start_hadoop.sh

--- lab_mapreduce_1/lab2/start_hadoop.sh
+++ lab_mapreduce_1/lab2/start_hadoop.sh
@@ -2,15 +2,10 @@
 
 hadoop fs -rm -r output/lab2/*
 
-hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
-    -Dmapreduce.job.reduces=1 \
-    -input input/lab_mapreduce_1/* \
-    -output output/lab2/allDocCount \
-    -mapper ~/projects/lab_mapreduce_1/lab2/mapper1.py \
-    -reducer ~/projects/lab_mapreduce_1/lab2/reducer1.py \
+
 
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
-    -Dmapreduce.job.reduces=1 \
+    -cacheFile output/lab2/allDocCount#AllDocCount
     -input input/lab_mapreduce_1/* \
     -output output/lab2/result \
     -mapper ~/projects/lab_mapreduce_1/lab2/mapper2.py \
