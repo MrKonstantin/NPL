e4bd8494054977032cdbc2eda04e8400e7d74c6d
MrKonstantin <mr.konstk@gmail.com>
2015-03-19 19:21:28 -0700

    lab2

--- lab_mapreduce_1/lab2/start_hadoop.sh
+++ lab_mapreduce_1/lab2/start_hadoop.sh
@@ -12,7 +12,7 @@ hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
     -reducer ~/projects/lab_mapreduce_1/lab2/reducer1.py \
 
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
-    -files hdfs://127.0.0.1:8020/user/cloudera/output/lab2/allDocCount/*#totalDoc \
+    -files hdfs://127.0.0.1:8020/user/cloudera/output/lab2/allDocCount/part-00000#totalDoc \
     -input input/lab_mapreduce_1/* \
     -output output/lab2/result \
     -mapper ~/projects/lab_mapreduce_1/lab2/mapper2.py \
