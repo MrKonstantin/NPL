96b31450a42812706c182a7b650f5ca4af9448fa
kkiselev <mr.konstk@gmail.com>
2015-03-20 20:34:56 +0300

    lab2

--- lab_mapreduce_1/lab2/start_hadoop.sh
+++ lab_mapreduce_1/lab2/start_hadoop.sh
@@ -2,10 +2,15 @@
 
 hadoop fs -rm -r output/lab2/*
 
-
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+    -Dmapreduce.job.reduces=1 \
+    -input input/lab_mapreduce_1/* \
+    -output output/lab2/allDocCount \
+    -mapper ~/projects/lab_mapreduce_1/lab2/mapper1.py \
+    -reducer ~/projects/lab_mapreduce_1/lab2/reducer1.py \
 
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
-    -cacheFile output/lab2/allDocCount#AllDocCount
+    -files hdfs://127.0.0.1:8020/user/cloudera/output/lab2/allDocCount#AllDocCount \
     -input input/lab_mapreduce_1/* \
     -output output/lab2/result \
     -mapper ~/projects/lab_mapreduce_1/lab2/mapper2.py \
