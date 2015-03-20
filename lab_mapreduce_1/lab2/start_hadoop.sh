0e06d3f3a8b04b4f144272311ba6302ba2274035
kkiselev <mr.konstk@gmail.com>
2015-03-20 04:01:07 +0300

    lab2

--- lab_mapreduce_1/lab2/start_hadoop.sh
+++ lab_mapreduce_1/lab2/start_hadoop.sh
@@ -6,7 +6,14 @@ hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
     -Dmapreduce.job.reduces=1
     -input input/lab_mapreduce_1/* \
     -output output/lab2/allDocCount \
-    -mapper ~/projects/lab_mapreduce_1/lab2/mapper.py \
-    -reducer ~/projects/lab_mapreduce_1/lab2/mapper.py \
+    -mapper ~/projects/lab_mapreduce_1/lab2/mapper1.py \
+    -reducer ~/projects/lab_mapreduce_1/lab2/reducer1.py \
+
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+    -Dmapreduce.job.reduces=1
+    -input input/lab_mapreduce_1/* \
+    -output output/lab2/result \
+    -mapper ~/projects/lab_mapreduce_1/lab2/mapper2.py \
+    -reducer ~/projects/lab_mapreduce_1/lab2/mapper2.py \
 
 hadoop fs -cat output/lab2/result/* | head > head_result.dat
