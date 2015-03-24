cb5fe3990d2c87453d9fdf70e8806146c6d08be3
kkiselev <mr.konstk@gmail.com>
2015-03-25 00:38:00 +0300

    Centroid

---
+++ lab_mapreduce_1/Centroid/init.sh
@@ -0,0 +1,4 @@
+
+chmod +x mapper1.py
+chmod +x reducer1.py
+chmod +x start_hadoop.sh
---
+++ lab_mapreduce_1/Centroid/start_hadoop.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+hadoop fs -rm -r output/lab_mapreduce_1/Centroid
+
+# Расчет количества документов
+# && Расчет IDF
+# && Вывод первых строк результата в файл
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+    -input input/lab_mapreduce_1/TFIDF/result/* \
+    -output output/lab_mapreduce_1/Centroid \
+    -mapper ~/projects/lab_mapreduce_1/Centroid/mapper1.py \
+    -reducer ~/projects/lab_mapreduce_1/Centroid/reducer1.py \
+&& hadoop fs -cat output/lab_mapreduce_1/Centroid/* | head > head_result.dat
