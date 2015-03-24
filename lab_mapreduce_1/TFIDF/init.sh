48a5ef0f4edbdd5a69534c858378ea144d410b21
kkiselev <mr.konstk@gmail.com>
2015-03-24 03:24:54 +0300

    TFIDF

--- lab_mapreduce_1/TFIDF/init.sh
+++ lab_mapreduce_1/TFIDF/init.sh
@@ -1,2 +1,3 @@
 
+hadoop fs -rm -r output/TFIDF
 hadoop fs -mkdir output/TFIDF
--- lab_mapreduce_1/TFIDF/start_hadoop.sh
+++ lab_mapreduce_1/TFIDF/start_hadoop.sh
@@ -3,19 +3,17 @@
 hadoop fs -rm -r output/lab_mapreduce_1/TFIDF/*
 
 # Расчет количества документов
+# && Расчет IDF
+# && Вывод первых строк результата в файл
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
-    -input input/lab_mapreduce_1/* \
-    -input output/lab_mapreduce_1/IDF/* \
+    -input input/lab_mapreduce_1/data.json \
+    -input output/lab_mapreduce_1/IDF/result/* \
     -output output/lab_mapreduce_1/TFIDF/mapreduce1 \
-    -mapper ~/projects/lab_mapreduce_1/TFIDF/mapper.py \
-    -reducer ~/projects/lab_mapreduce_1/TFIDF/reducer.py
-
-# Расчет IDF
-hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+    -mapper "python3 mapper.py" \
+    -reducer "python3 reducer.py" \
+&& hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
     -input output/lab_mapreduce_1/TFIDF/mapreduce1/* \
     -output output/lab_mapreduce_1/TFIDF/result \
-    -mapper ~/projects/lab_mapreduce_1/TFIDF/mapper2.py \
-    -reducer ~/projects/lab_mapreduce_1/TFIDF/reducer2.py
-
-# Вывод первых строк результата в файл
-hadoop fs -cat output/lab_mapreduce_1/TFIDF/result/* | head > head_result.dat
+    -mapper "python3 mapper2.py" \
+    -reducer "python3 reducer2.py" \
+&& hadoop fs -cat output/lab_mapreduce_1/TFIDF/result/* | head > head_result.dat
