aa9ba60891a6da9cee5b75781817e191b595c52a
kkiselev <mr.konstk@gmail.com>
2015-03-24 03:21:03 +0300

    TFIDF

--- lab_mapreduce_1/IDF/start_hadoop.sh
+++ lab_mapreduce_1/IDF/start_hadoop.sh
@@ -3,16 +3,14 @@
 hadoop fs -rm -r output/lab_mapreduce_1/IDF
 hadoop fs -rm -r output/lab_mapreduce_1/allDocCount
 
-# Расчет количества документов
+# Расчет количества документов && Расчет IDF
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
     -Dmapreduce.job.reduces=1 \
     -input input/lab_mapreduce_1/data.json \
     -output output/lab_mapreduce_1/allDocCount \
     -mapper ~/projects/lab_mapreduce_1/IDF/mapper1.py \
     -reducer ~/projects/lab_mapreduce_1/IDF/reducer1.py \
-
-# Расчет IDF
-hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+&& hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
     -files hdfs://127.0.0.1:8020/user/cloudera/output/IDF/allDocCount/part-00000#totalDoc \
     -input input/lab_mapreduce_1/data.json \
     -output output/lab_mapreduce_1/IDF \
