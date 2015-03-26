1f15f30664c50ea78414a5e1cdbc4716af27d025
kkiselev <mr.konstk@gmail.com>
2015-03-26 05:41:58 +0300

    Centroid

--- lab2_superachiev/start_hadoop.sh
+++ lab2_superachiev/start_hadoop.sh
@@ -11,6 +11,7 @@ hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
         -mapper "python mapper1.py"
         -reducer "python reducer1.py"\
 && hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
+        -Dmapreduce.job.reduces=1 \
         -file mapper.py \
         -input s3n://newprolab/facetz_2015_02_0[1-5]\
         -output user/konstantin.kiselev/top350.txt \
