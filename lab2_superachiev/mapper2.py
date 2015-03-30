7237cf09f5fe033fd35e91281ab79a004af1483a
kkiselev <mr.konstk@gmail.com>
2015-03-31 00:44:25 +0300

    Hadoop lab2 top350 url

--- lab2_superachiev/mapper2.py
+++ lab2_superachiev/mapper2.py
@@ -1,11 +1,12 @@
 import sys
 
+maxint = 0
+if sys.version_info.major == 2:
+	maxint = sys.maxint
+else:
+	maxint = sys.maxsize
+
 for line in sys.stdin:
 	url, count = line.strip().split('\t')
-	t = - int(count)
-	if sys.version_info.major == 2:
-		t += sys.maxint
-	else:
-		t += sys.maxsize
-
+	t = maxint - int(count)
 	print(str(t) + '\t' + count + '\t' + url)
