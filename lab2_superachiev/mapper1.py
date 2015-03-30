e2c7dbd2e0630185ebb3179a79750555709f6281
kkiselev <mr.konstk@gmail.com>
2015-03-31 00:30:24 +0300

    Hadoop lab2 top350 url

--- lab2_superachiev/mapper1.py
+++ lab2_superachiev/mapper1.py
@@ -1,7 +1,11 @@
 from sys import stdin
 
 for line in stdin:
-	split = line.strip().split('\t')
-	if len(split) == 3 and split[0] != '-' and split[2] != '':
+	try:
+		split = line.strip().split('\t')
+	except:
+		continue
+
+	if len(split) == 3 and split[0] != '-':
 		url = split[2]
 		print(url)
--- lab2_superachiev/mapper2.py
+++ lab2_superachiev/mapper2.py
@@ -2,5 +2,10 @@ import sys
 
 for line in sys.stdin:
 	url, count = line.strip().split('\t')
-	t = sys.maxint - int(count)
+	t = - int(count)
+	if sys.version_info.major == 2:
+		t += sys.maxint
+	else:
+		t += sys.maxsize
+
 	print(str(t) + '\t' + count + '\t' + url)
