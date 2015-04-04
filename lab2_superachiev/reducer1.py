ca42b0565cda50e176beabb943ee8cc265d876f4
Konstantin <mr.konstk@gmail.com>
2015-04-05 02:15:26 +0300

    Update reducer1.py

--- lab2_superachiev/reducer1.py
+++ lab2_superachiev/reducer1.py
@@ -1,17 +1,34 @@
 from sys import stdin
+from heapq import heappush, heappop
 
+url = None
 prev = None
 count = 0
+heap = []
+N = 350
+
 for line in stdin:
-	url = line.strip().split('\t')[0]
-	if prev is None:
-		prev = url
+        url = line.strip().split('\t')[0]
+        if prev is None:
+                prev = url
 
-	if prev != url:
-		print(url + '\t' + str(count))
-		count = 1
-		prev = url
-	else:
-		count += 1
+        if prev != url:
+                #print(prev + '\t' + str(count))
+                heappush(heap, (count, prev))
+                if len(heap) > N:
+                        heappop(heap)
 
-print(prev + '\t' + str(count))
+                count = 1
+                prev = url
+        else:
+                count += 1
+
+if prev == url:
+        #print(prev + '\t' + str(count))
+        heappush(heap, (count, prev))
+        if len(heap) > N:
+                heappop(heap)
+
+while heap:
+        c, u = heappop(heap)
+        print(u + '\t' + str(c))
