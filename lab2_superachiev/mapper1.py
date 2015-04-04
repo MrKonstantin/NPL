49365b6b8911f0b100cf8e8623d01e7ef2f1d0e0
Konstantin <mr.konstk@gmail.com>
2015-04-05 02:14:35 +0300

    Update mapper1.py

--- lab2_superachiev/mapper1.py
+++ lab2_superachiev/mapper1.py
@@ -1,11 +1,11 @@
 from sys import stdin
 
 for line in stdin:
-	try:
-		split = line.strip().split('\t')
-	except:
-		continue
+        try:
+                split = line.strip().split('\t')
+        except:
+                continue
 
-	if len(split) == 3 and split[0] != '-':
-		url = split[2]
-		print(url + '\t' + '1')
+        if len(split) == 3 and split[0] != '-':
+                url = split[2]
+                print(url + "\t" + "1")
