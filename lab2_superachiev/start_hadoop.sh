2b74ccf19e12ddd70675ba2d0acaadafdf06c41d
Konstantin <mr.konstk@gmail.com>
2015-04-05 14:52:36 +0300

    Update start_hadoop.sh

--- lab2_superachiev/start_hadoop.sh
+++ lab2_superachiev/start_hadoop.sh
@@ -6,5 +6,5 @@ hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
         -input s3n://newprolab/facetz_2015_02_03/* \
         -output output/lab2_sa/mapreduce1 \
         -mapper "python mapper1.py" \
-        -reducer "python reducer1.py"
-&& hadoop  fs -cat ouput/lab2_sa/mapreduce1 | sort -rnk2 | head -n350 > top350.txt
+        -reducer "python reducer1.py" \
+&& hadoop  fs -cat ouput/lab2_sa/mapreduce1/* | sort -rnk2 | head -n350 > top350.txt
