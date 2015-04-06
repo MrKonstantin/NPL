df9d10b1edf3b3738d43804b4fd2427c29c04453
Konstantin <mr.konstk@gmail.com>
2015-04-06 14:44:23 +0300

    Update start_hadoop.sh

--- lab3s/start_hadoop.sh
+++ lab3s/start_hadoop.sh
@@ -4,14 +4,14 @@ rm auto_number.txt
 
 hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
         -files mapper1.py,count_auto.py,autousers_lab3s.txt \
-        -input s3n://newprolab/lab3data/part-0000[0-18] \
+        -input s3n://newprolab/lab3data/* \
         -output output/lab3s/count_auto.txt \
         -mapper "python mapper1.py" \
         -reducer "python count_auto.py"\
 && hadoop fs -cat output/lab3s/count_auto.txt/* > auto_number.txt\
 && hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
         -files mapper1.py,reducer1.py,autousers_lab3s.txt,auto_number.txt \
-        -input s3n://newprolab/lab3data/part-0000[0-18] \
+        -input s3n://newprolab/lab3data/* \
         -output output/lab3s/lab3Sdomains.txt \
         -mapper "python mapper1.py" \
         -reducer "python reducer1.py"\
